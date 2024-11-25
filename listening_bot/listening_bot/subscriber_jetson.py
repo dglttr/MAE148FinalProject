24982b32e7e41a203769dcbc906900ba03de6f7b
Daniel Glatter <amkcpu@outlook.com>
2024-11-25 15:14:26 -0800

    Log range data

--- listening_bot/listening_bot/subscriber_jetson.py
+++ listening_bot/listening_bot/subscriber_jetson.py
@@ -74,6 +74,7 @@ class SteeringCommandSubscriber(Node):
         front_ranges = ranges[start_idx:end_idx]
         min_distance = min(front_ranges)
         self.get_logger().info(f"Min. distance: {min_distance}")
+        self.get_logger().info(f'Ranges: {ranges}')
 
         too_close = True if min_distance <= MIN_ALLOWED_DISTANCE else False
 
